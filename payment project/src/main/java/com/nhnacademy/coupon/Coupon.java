f620aa909919e97e4ef77b395d69a4b8eef4f2cb
ChanYoungHong <cksdud9883@naver.com>
2022-04-07 00:59:41 +0900

    1000원 쿠폰 테스트 완료

--- payment project/src/main/java/com/nhnacademy/coupon/Coupon.java
+++ payment project/src/main/java/com/nhnacademy/coupon/Coupon.java
@@ -1,24 +1,31 @@
 package com.nhnacademy.coupon;
 
+import com.nhnacademy.exceptions.AmountTargetIsMinusException;
+
 public enum Coupon implements Discountable  {
     ONE_THOUSON(1000, 0){
         @Override
         public int discount(int balance) {
+            if(balance < 0){
+                throw new AmountTargetIsMinusException("가격이 음수면 할인이 불가능합니다.");
+            }
+            if(balance < 1000){
+                return 0;
+            }
             return balance - this.amt;
         }
-
     },
     TEN(0, 0.1) {
         @Override
         public int discount(int balance) {
-            return (int) (balance - this.rate);
+            return 0;
         }
     },
 
     TWENTY(0, 0.2){
         @Override
         public int discount(int balance){
-            return (int)(balance * this.rate);
+            return 0;
         }
     }
     ;
--- payment project/src/test/java/com/nhnacademy/coupon/CouponTest.java
+++ payment project/src/test/java/com/nhnacademy/coupon/CouponTest.java
@@ -1,7 +1,9 @@
 package com.nhnacademy.coupon;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.nhnacademy.exceptions.AmountTargetIsMinusException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,7 +16,24 @@ public class CouponTest {
         coupon = Coupon.ONE_THOUSON;
         int balance = 1500;
         assertThat(coupon.discount(balance)).isEqualTo(500);
+    }
 
+    @DisplayName("금액이 음수면 AmountTargetIsMinusException 던짐")
+    @Test
+    void apply_balance_is_negative_number(){
+        coupon = Coupon.ONE_THOUSON;
+        int balance = -1500;
+        assertThatThrownBy(() -> coupon.discount(balance))
+            .isInstanceOf(AmountTargetIsMinusException.class)
+            .hasMessageContaining("음수", "할인");
+    }
+
+    @DisplayName("천 원보다 작을 때 최종금액은 0원")
+    @Test
+    void apply_balance_is_under_thousand(){
+        coupon = Coupon.ONE_THOUSON;
+        int balance = 500;
+        assertThat(coupon.discount(balance)).isEqualTo(0);
     }
 
 }
