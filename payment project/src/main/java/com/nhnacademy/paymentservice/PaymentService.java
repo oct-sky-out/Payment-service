9437ff0498648d2ff8a7220aa7a8973637c06978
ChanYoungHong <cksdud9883@naver.com>
2022-04-06 18:30:33 +0900

    유저ID를 이용한 account 가져오기 테스트 작성

--- payment project/src/main/java/com/nhnacademy/paymentservice/PaymentService.java
+++ payment project/src/main/java/com/nhnacademy/paymentservice/PaymentService.java
@@ -1,14 +1,23 @@
 package com.nhnacademy.paymentservice;
 
 
+import com.nhnacademy.accountrepository.AccountRepository;
 import com.nhnacademy.exceptions.AmountTargetIsMinusException;
 import com.nhnacademy.receipt.Receipt;
 
 public class PaymentService {
+    private final AccountRepository repo;
+
+    public PaymentService(AccountRepository repo) {
+        this.repo = repo;
+    }
 
     public void pay(int amount, int customerId) {
         if (amount < 0){
             throw new AmountTargetIsMinusException("결제 금액이 음수입니다.");
         }
+        this.repo.getAccountById(customerId);
     }
+
+
 }
--- payment project/src/test/java/com/nhnacademy/paymentservice/PaymentServiceTest.java
+++ payment project/src/test/java/com/nhnacademy/paymentservice/PaymentServiceTest.java
@@ -3,28 +3,46 @@ package com.nhnacademy.paymentservice;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.nhnacademy.account.Account;
+import com.nhnacademy.accountrepository.AccountRepository;
 import com.nhnacademy.exceptions.AccountAccessFailException;
 import com.nhnacademy.exceptions.AmountTargetIsMinusException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PaymentServiceTest {
     PaymentService service;
+    AccountRepository repo;
 
     @BeforeEach
     void setUp() {
-        service = new PaymentService();
+        repo = mock(AccountRepository.class);
+        service = new PaymentService(repo);
     }
 
     @Test
     void pay_amount_is_minus() {
         int amount = -1000;
         int accountId  = 1;
-       assertThatThrownBy(()->service
+       assertThatThrownBy(() -> service
            .pay(amount,accountId))
            .isInstanceOf(AmountTargetIsMinusException.class)
            .hasMessageContaining("결제 금액","음수");
+    }
+
+    @Test
+    @DisplayName("customer id로 account정보를 가져온다")
+    void get_account_by_customer_id(){
+        int id = 0;
+        Account account = new Account(id);
+        when(repo.getAccountById(id)).thenReturn(account);
+        // repo.getAccountById(id)를 호출하면 account를 리턴한다.
+        service.pay(500,id);
+        verify(repo).getAccountById(id);
+        // 검증한다. repo를, getAccountById(id);를 호출했니?
     }
 }
